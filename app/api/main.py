3362221aa1042538250e287393ac61dfae6b00ca
Jamova01 <jorgemova01@gmail.com>
2025-11-28 19:23:26 -0500

    feat(api): register auth router in main API router

--- app/api/main.py
+++ app/api/main.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
-from app.api.routes import users
+
+from app.api.routes import auth, users
+
 
 api_router = APIRouter()
 
 # Register all routers here
 api_router.include_router(users.router)
+api_router.include_router(auth.router)

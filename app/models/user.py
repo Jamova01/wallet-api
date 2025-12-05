3504f6690ca0f3d0fb13339c43c6b7a8c78fda0f
Jamova01 <jorgemova01@gmail.com>
2025-12-05 18:34:39 -0500

    feat(models): add RefreshToken relationship to User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,25 +1,18 @@
-import uuid
-from datetime import datetime, timezone
+from uuid import UUID, uuid4
+from typing import TYPE_CHECKING, List
 from sqlmodel import Relationship, SQLModel, Field
 from pydantic import EmailStr
 
-from app.models.token import RefreshToken
+
+if TYPE_CHECKING:
+    from app.models import RefreshToken
 
 
 class User(SQLModel, table=True):
-    __tablename__ = "users"
-
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    email: EmailStr = Field(unique=True, index=True, max_length=255)
-    full_name: str | None = Field(default=None, max_length=255)
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    email: EmailStr = Field(unique=True, index=True)
     hashed_password: str
     is_active: bool = Field(default=True)
     is_superuser: bool = Field(default=False)
-    created_at: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc), nullable=False
-    )
-    updated_at: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc), nullable=False
-    )
 
-    refresh_tokens: list["RefreshToken"] = Relationship(back_populates="user")
+    refresh_tokens: List["RefreshToken"] = Relationship(back_populates="user")
